4bd8520b2f358fba952054602a875aabb92bf3fd
jck-bit <jckkinyanjui@gmail.com>
2021-10-11 12:11:37 +0300

    starting on routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import Todo from "./components/Todo";
-
-function App() {
-    return (
-      <div>
-        <h1>Learn React</h1>
-        <Todo text ='learn react' />
-        <Todo text ='master react'/>
-        <Todo text='Finish react course'/>
-      </div>
-       
-  );
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
