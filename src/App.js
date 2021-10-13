910d01b2b00d65744d85773edd44528ed7d42475
jck-bit <jckkinyanjui@gmail.com>
2021-10-13 21:33:39 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import { Route,Switch } from 'react-router-dom'
 import AllmeetupsPage from './pages/Allmeetups'
 import NewMeetupPage from './pages/NewMeetup'
 import Favoritespage from './pages/Favorites'
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout'
 
 function App() {
 //localhost:3000/
 
   return (
-    <div>
-     <MainNavigation /> 
+    <Layout>
      <Switch>
       <Route path = '/' exact>
         <AllmeetupsPage />
       </Route>
       <Route path = '/new-meetups'>
-        <NewMeetupPage/>
+        <NewMeetupPage />
       </Route>
       <Route path = '/favorites'>
         <Favoritespage />
       </Route>
      </Switch>
-    </div>
+    </Layout>
   )
 }
 
